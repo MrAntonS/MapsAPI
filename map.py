1e57a90f31b0540cd844cf33051d7ea35a3405c6
Gluhov Kirill <34452390+GluKhovKirill@users.noreply.github.com>
2019-03-18 17:10:14 +0300

    Add files via upload

--- map.py
+++ map.py
@@ -2,10 +2,10 @@ import requests
 
 
 geocoder_api_server = "http://static-maps.yandex.ru/1.x/"
-def get_map(x, y, zoom, filename):
-    params = {"l": "skl",
-                       "z": str(zoom),
-                       "ll": ",".join([str(x),str(y)])}
+def get_map(x, y, zoom, filename, map_type="skl"):
+    params = {"l": map_type,
+              "z": str(zoom),
+              "ll": ",".join([str(x),str(y)])}
     response = requests.get(geocoder_api_server, params=params)
     if not response:
         err = "Http статус:"+str(response.status_code)+"("+str(response.reason)+")"
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     x, y = '37.606281', '55.822878'
     zoom = 15
     filename = "map.png"
-    print(get_map(x, y, zoom, filename) )
+    print(get_map(x, y, zoom, filename, "sat") )
