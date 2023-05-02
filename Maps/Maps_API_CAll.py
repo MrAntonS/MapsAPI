5f68349b0d6dbe8c418c6025f5fef2875712fb13
Anton <noreply@gmail.com>
2023-05-02 11:01:16 -0400

    ver1.0

--- Maps/Maps_API_CAll.py
+++ Maps/Maps_API_CAll.py
@@ -6,8 +6,10 @@ from map import get_map, bbox
  
 response = None
 try:
-    x = input().split()
-    map_file = get_map(x[0],x[1],x[2],x[3])
+    args = ["X coordinate: ", "Y coordinate: ", "Zoom: "]
+    args = list(map(lambda x: int(input(x)), args))
+    args.append('tmp.png')
+    map_file = get_map(x=args[0],y=args[1],zoom=args[2],filename=args[3])
 except IOError as ex:
     print("Ошибка записи временного файла:", ex)
     sys.exit(2)
@@ -17,31 +19,31 @@ pygame.init()
 screen = pygame.display.set_mode((450, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
 
-screen.blit(pygame.image.load(x[3]), (0, 0))
+screen.blit(pygame.image.load(args[3]), (0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
 running = True
-x[2] = int(x[2])
+args[2] = int(args[2])
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYUP:
-            print(x[2])
+            print(args[2])
             if event.key == 280: 
-                if x[2] != 20:
-                    x[2] += 1
+                if args[2] != 20:
+                    args[2] += 1
             if event.key == 281:
-                if x[2] != 0:
-                    x[2] -= 1
+                if args[2] != 0:
+                    args[2] -= 1
             if event.key == 273:
-                bbox([[x[0],x[1]+2],[[x[0],x[1]+4]]])
-    x[2] = abs(x[2])
-    map_file = get_map(x[0],x[1],x[2],x[3])
-    screen.blit(pygame.image.load(x[3]), (0, 0))
+                bbox([[args[0],args[1]+2],[[args[0],args[1]+4]]])
+    args[2] = abs(args[2])
+    map_file = get_map(args[0],args[1],args[2],args[3])
+    screen.blit(pygame.image.load(args[3]), (0, 0))
     pygame.display.flip()
     pass
 pygame.quit()
  
 # Удаляем за собой файл с изображением.
-os.remove(x[3])
+os.remove(args[3])
